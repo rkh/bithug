5dfc3d8f6bfd57cd472952807e782f61c49addb1
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-06-24 14:05:08 +0200

    it's expand_path

--- bithug.rb
+++ bithug.rb
@@ -1,5 +1,5 @@
 require 'pathname'
-$LOAD_PATH << Pathname("./lib").expand.to_s << Dir.glob(Pathname("./vendor/*/lib").expand)
+$LOAD_PATH << Pathname("./lib").expand_path.to_s << Dir.glob(Pathname("./vendor/*/lib").expand_path)
 
 require 'sinatra'
 require 'haml'
