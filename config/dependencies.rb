075a9e9ef3008deed3e6a9d0ef2d92a8f1418a84
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-06-24 16:07:04 +0200

    fixed dependency issues

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -2,11 +2,17 @@ require 'pathname'
 
 module Bithug
   ROOT = Pathname(__FILE__).dirname.join('..').expand_path
-  $LOAD_PATH.unshift ROOT.join("lib").to_s, *Dir.glob(ROOT.join("vendor/"))
-end
+  $LOAD_PATH.unshift ROOT.join("lib").to_s, *Dir.glob(ROOT.join("vendor/**/lib").to_s)
+end 
 
-%w[
+require "pp"
+pp $LOAD_PATH
+
+%w[                      
+  dm-core dm-aggregates dm-migrations dm-timestamps dm-types dm-validations dm-serializer 
   data_objects do_sqlite3
-  dm-core dm-aggregates dm-migrations dm-timestamps dm-types dm-validations dm-serializer
+  grit erubis haml
   bithug
 ].each { |lib| require lib }
+
+DataMapper.setup :default, "sqlite3:///#{Bithug::ROOT}/#{Bithug.environment}.db" 
