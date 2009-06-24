673413b2547d32821b3c08da3bc0673c606efcfc
Tim Felgentreff <tim@medion.(none)>
2009-06-24 15:15:23 +0200

    add my specs for user and repos

---
+++ spec/repository_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Bithug::Repository do
+  before do
+    @repos = Bithug::Repository.new
+  end
+
+  it "has a name" do
+    repos.should.respond_to?(:name)
+    repos.reposname.should.is_a?(String)
+  end
+
+  it "has some members" do
+    repos.should.respond_to?(:members)
+    repos.members.should.is_a?(Array)
+  end
+end
