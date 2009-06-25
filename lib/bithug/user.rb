47515a17fc29b0bcbdbbb381abbf896861eef749
timfel <timfelgentreff@gmail.com>
2009-06-25 13:56:57 +0200

    add a password field

--- lib/bithug/user.rb
+++ lib/bithug/user.rb
@@ -5,11 +5,12 @@ module Bithug
     include DataMapper::Resource
     
     property :name, String, :key => true
+    property :password, String
     property :created_at, DateTime
     property :updated_at, DateTime
 
     has n, :repositories
-    has n, :keys
+    #has n, :keys, :class_name => 'Bithug::PublicKey'
     
   end
   
