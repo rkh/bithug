b5cbe4231564821b9df3e1c10ade92022e1ed4c9
timfel <timfelgentreff@gmail.com>
2009-06-26 15:38:25 +0200

    provide register

--- webserver.rb
+++ webserver.rb
@@ -8,3 +8,19 @@ include Bithug
 get "/" do
   haml :index, {}, :user => User.all
 end
+
+get "/register" do
+  haml :register
+end
+
+post "/register" do
+  options.authentication_agent.
+    register params[:name], params[:password]
+  redirect "/"
+end
+
+post"/login" do
+  res = options.authentication_agent.
+    athenticate params[:name], params[:password]
+  res.to_s
+end
