75578c7fe4c9b852dd6ca0350d2ccb82ab1fc010
timfel <timfelgentreff@gmail.com>
2009-06-26 15:36:21 +0200

    this has to be cleaned up

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -5,11 +5,13 @@ module Bithug
   $LOAD_PATH.unshift ROOT.join("lib").to_s, *Dir.glob(ROOT.join("vendor/**/lib").to_s)
 end
 
-%w[                      
+%w[
   dm-core dm-aggregates dm-migrations dm-timestamps dm-types dm-validations dm-serializer 
   data_objects do_sqlite3 net/ldap bcrypt
-  sinatra/base grit erubis haml
-  bithug
+  sinatra/base grit erubis haml 
+  bithug bithug/authentication/local_authentication
 ].each { |lib| require lib }
 
 DataMapper.setup :default, "sqlite3:///#{Bithug::ROOT}/#{Bithug.environment}.db"
+
+Sinatra::Default.set :authentication_agent, LocalAuthenticationAgent.new
