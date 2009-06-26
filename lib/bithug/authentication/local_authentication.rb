05e80836fe30ea81841544dd71407947ac561d33
timfel <timfelgentreff@gmail.com>
2009-06-26 15:37:54 +0200

    better to take those args

--- lib/bithug/authentication/local_authentication.rb
+++ lib/bithug/authentication/local_authentication.rb
@@ -1,11 +1,17 @@
+require 'bithug'
 require 'bcrypt'
 
 # This agent authenticates against the 
 # local database
-class LocalAuthenticationAgent
+ class LocalAuthenticationAgent
   def authenticate username, password
     user = User.get(username)
     return nil if user.nil?
     password = BCrypt::Password.new(user.password)
+  end
+
+  def register(username, password)
+    User.create!(:name => username,
+      :password => password)
   end
 end
