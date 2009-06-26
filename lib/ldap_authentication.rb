7a3f0b2b7d24a6c014cc18dd1b99c2cc9a1bd10b
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-06-26 14:00:30 +0200

    minor fixes

--- lib/ldap_authentication.rb
+++ lib/ldap_authentication.rb
@@ -1,12 +1,12 @@
 require 'net/ldap'
 
 class LDAPAuthenticationAgent
-  def initialize options
+  def initialize(options)
     @ldap = Net::LDAP.new :host => "nads1",
       :port => 389
   end
 
-  def authenticate username, password
+  def authenticate(username, password)
     @ldap.auth username, password
     @ldap.bind
   end
--- lib/local_authentication.rb
+++ lib/local_authentication.rb
@@ -3,7 +3,7 @@ require 'bcrypt'
 # This agent authenticates against the 
 # local database
 class LocalAuthenticationAgent
-  def authenticate username, password
+  def authenticate(username, password)
     user = User.get(username)
     return nil if user.nil?
     password = BCrypt::Password.new(user.password)
