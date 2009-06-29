e4e9f6352eba005636de20896fa8e46f6d6b28aa
timfel <timfelgentreff@gmail.com>
2009-06-29 10:27:15 +0200

    add some routes and cookie-based sessions

--- webserver.rb
+++ webserver.rb
@@ -5,8 +5,10 @@ require 'haml'
 
 include Bithug
 
+enable :sessions
+
 get "/" do
-  haml :index, {}, :user => User.all
+  haml :index, {}, :user => User.get(session["username"])
 end
 
 get "/register" do
@@ -19,8 +21,29 @@ post "/register" do
   redirect "/"
 end
 
-post"/login" do
-  res = options.authentication_agent.
+post "/login" do
+  if options.authentication_agent.
     authenticate params[:name], params[:password]
-  res.to_s
+    session["username"] = params[:name]
+  end
+  redirect "/"
+end
+
+get "/logout" do
+  session["username"] = nil
+  redirect "/"
+end
+
+get "/:username" do
+  haml :index, {}, :user => User.get(params[:username])
+end
+
+get "/create_repository" do
+  haml :repository
+end
+
+post "/create_repository" do
+  users = params[:userlist].gsub(" ", "").split(",")
+  Repository.new.update(session["username"], 
+                        params[:name], users)
 end
