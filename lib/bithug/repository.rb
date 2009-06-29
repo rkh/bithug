a89ffb7be5b3f1ab2660ea10f2a111dd9229fdf8
timfel <timfelgentreff@gmail.com>
2009-06-29 10:26:49 +0200

    add preliminary creation method

--- lib/bithug/repository.rb
+++ lib/bithug/repository.rb
@@ -13,6 +13,17 @@ module Bithug
     belongs_to :user, :class_name => 'User', :child_key => [:name]
     
     alias_method :owner, :user
+
+    def update owner, name, users
+      owner = User.get(owner)
+      users = users.collect {|u| User.get(u)}.compact
+      r = Repository.get(name) || Repository.new(:name => name)
+      r.attributes = {:members => users, 
+        :created_at => Time.now}
+      r.save
+      owner.repositories << r
+      owner.save
+    end
     
   end
   
