3fca8c72ab8f09d36e957dd876d940e180602f1a
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-09-08 18:32:29 +0200

    display readme on /

--- routes/home.rb
+++ routes/home.rb
@@ -2,7 +2,10 @@ module Project
   class Routes < Monk::Glue
 
     get '/' do
-      haml :home
+      require 'rdoc/markup/to_html'
+      h = RDoc::Markup::ToHtml.new
+      src = h.convert(File.read(root_path("README.rdoc")))
+      haml :home, {}, :rdoc => src
     end
 
   end
