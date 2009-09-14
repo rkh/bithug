7bf4ccebeff5e0efbcbfea0ea368b898d245043a
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-09-14 17:18:20 +0200

    move routes specs to subfolder

--- spec/project/routes/stylesheets_spec.rb
+++ spec/project/routes/stylesheets_spec.rb
@@ -1,5 +1,5 @@
 # require_relative "../spec_helper.rb" in ruby 1.9
-require File.expand_path(File.join(File.dirname(__FILE__), "..", "spec_helper.rb"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper.rb"))
 
 describe Project::Routes do
   
