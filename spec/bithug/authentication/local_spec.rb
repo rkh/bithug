1ea60eb2a537517da1cbaf7e0e9f5395c1491a4d
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-09-22 16:51:59 +0200

    spec still fails, dunno why

---
+++ spec/bithug/authentication/local_spec.rb
@@ -0,0 +1,2 @@
+# require_relative "../spec_helper.rb" in ruby 1.9
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper.rb"))
--- spec/bithug/routes/stylesheets_spec.rb
+++ spec/bithug/routes/stylesheets_spec.rb
@@ -4,6 +4,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_hel
 describe Bithug::Routes do
   
   it "returns css for /stylesheets/screen.css" do
+    logged_in
     get "/stylesheets/screen.css"
     last_response.should be_ok
     last_response.content_type.should =~ /^text\/css/
