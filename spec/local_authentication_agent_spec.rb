3da8a3330a6d23cbecdd7dc801ae83b370c8915f
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-09-22 10:47:50 +0200

    add pending specs
    Conflicts:
    
    	config/dependencies.rb

---
+++ spec/local_authentication_agent_spec.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe LocalAuthenticationAgent do
+  it "encrypts passwords" do
+    pending
+  end
+
+  it "authenticates users safely" do
+    pending
+  end
+end
