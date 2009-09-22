b98334b5b6a2d6ec861175957395308f77d343bc
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-09-22 11:09:26 +0200

    some more clean-up

---
+++ lib/bithug/authentication/kerberos.rb
@@ -0,0 +1,36 @@
+require 'krb5_auth'
+
+module Bithug
+  module Authentication
+
+    # Add this to /etc/krb5.conf
+    #
+    # [libdefaults]
+    # default_realm = HPI.UNI-POTSDAM.DE
+    #
+    # [realms]
+    # HPI.UNI-POTSDAM.DE = {
+    #     kdc = 141.89.221.28
+    #     admin_server = 141.89.221.28
+    # }
+    class Kerberos
+
+      def initialize(options=nil)
+        @krb5 = Krb5Auth::Krb5.new
+      end
+
+      def authenticate(username, password)
+        begin
+          @ldap.get_init_creds_password(username, password)
+        rescue Krb5Auth::Krb5::Exception
+          false
+        end
+      end
+
+      [:register, :update].each do |item|
+        define_method(item) { nil }
+      end
+    end
+
+  end
+end
---
+++ lib/bithug/authentication/local.rb
@@ -0,0 +1,35 @@
+require 'bithug/user'
+require 'bcrypt'
+
+module Bithug
+  module Authentication
+
+    # This agent authenticates against the 
+    # local database
+    class Local
+
+      def authenticate(username, password)
+        user = User.get(username)
+        return nil if user.nil?
+        BCrypt::Password.new(user.password) == password
+      end
+
+      def register(username, password)
+        user = User.new
+        user.attributes = {:name => username, :password => password}
+        user.save!
+        user
+      end
+
+      def update(username, details)
+        if details[:password] == details[:repeated_password]
+          user = User.get(username) || register(username, details[:password])
+          user = nil unless user.update(details)
+        end
+        user
+      end
+
+    end
+
+  end
+end
