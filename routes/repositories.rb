7dcec78da3a0239e8754177ad1a5c9ebb1bf04d9
Tim Felgentreff <tim@bp2009h1srv.(none)>
2009-10-16 15:23:52 +0200

    add lots of helpers, optionalize a trailing slash and create simple view

--- routes/repositories.rb
+++ routes/repositories.rb
@@ -13,35 +13,51 @@ module Bithug
       def manager
         AccessManager.new(current_user)
       end
+
+      def path
+	current_user+"/"+params[:name]
+      end
+
+      def hostname
+	(ENV["HOSTNAME"] || %x["hostname"]).strip
+      end
+
+      def writers
+	Repository.find(:name, path).first.writeaccess.all.collect do |w|
+	  w.name
+	end << current_user
+      end
+
+      def readers
+	Repository.find(:name, path).first.readaccess.all.collect do |r|
+	  r.name
+	end
+      end
     end
 
     get "/repositories/?" do
       "list of repositories you have access to"
     end
 
-    get "/repositories/new" do
+    get "/repositories/new/?" do
        haml :"repositories/new"
     end
 
-    post "/repositories/new" do
-       reponame = params["post"]["name"]
-       manager.add_repository(params["name"])
-       redirect "/repositories/#{reponame}"
+    post "/repositories/new/?" do
+      reponame = params["post"]["name"]
+      manager.add_repository(params["name"])
+      redirect "/repositories/#{reponame}"
     end
 
-    post "/repositories/?" do
-      "create a new repo"
-    end
-
-    get "/repositories/:name" do
-      "show a repo"
+    get "/repositories/:name/?" do
+      haml :"repositories/view"
     end 
 
-    put "/repositories/:name" do
+    put "/repositories/:name/?" do
       "modify a repo"
     end
 
-    delete "/repositories/:name" do
+    delete "/repositories/:name/?" do
       "remove a repo"
     end
 
