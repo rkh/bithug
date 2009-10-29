648ec2b41b36dc9b4ef1ba66e97945ffdb32f508
Tim Felgentreff <timfelgentreff@gmail.com>
2009-10-29 17:35:11 +0100

    show the user's keys on the home view

--- routes/home.rb
+++ routes/home.rb
@@ -2,10 +2,7 @@ module Bithug
   class Routes < Sinatra::Base
 
     get '/' do      
-      require 'rdoc/markup/to_html'
-      h = RDoc::Markup::ToHtml.new
-      src = h.convert(File.read(root_path("README.rdoc")))
-      haml :home, {}, :rdoc => src
+      haml :home, {}, :user => current_user
     end
 
   end
