05a53693871bb1460239f3c28fab5371a0681101
Tim Felgentreff <tim@bp2009h1ws03.hpi.uni-potsdam.de>
2009-10-30 11:18:33 +0100

    add and show keys

--- routes/home.rb
+++ routes/home.rb
@@ -1,8 +1,19 @@
 module Bithug
   class Routes < Sinatra::Base
 
+    def manager
+      AccessManager.new(current_user)
+    end
+
+
     get '/' do      
-      haml :home, {}, :user => current_user
+      haml :home, {}, :user => User.find(:name, current_user).first
+    end
+
+    post "/" do
+      new_key = params["post"]["key"]
+      manager.add_key(new_key)
+      redirect "/"
     end
 
   end
