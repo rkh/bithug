d5b156ccf27b00dfc399039f68aadc7a41ce1f41
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-12-11 17:55:49 +0100

    load big_band

--- lib/bithug.rb
+++ lib/bithug.rb
@@ -1,4 +1,7 @@
-require "monkey/autoloader"
+# fix this as soon as monkey-lib has backend detection
+require "monkey"
+require "monkey/backend/active_support"
+require "big_band"
 
 class Bithug < BigBand
   extend Monkey::Autoloader
