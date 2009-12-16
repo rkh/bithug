03d2c35dae14ae54defb3d462e2c77b330e1e4f1
Tim Felgentreff <timfelgentreff@gmail.com>
2009-12-16 18:10:02 +0100

    create login and authenticate on class side, use Object-Assocs, ignogre problematic Ohm for now

--- lib/bithug/user.rb
+++ lib/bithug/user.rb
@@ -1,14 +1,28 @@
 require "bithug"
 
+# A user of Bithug - nice and pretty
 class Bithug::User < Ohm::Model
   attribute :name
-  set :following
-  set :followers
+  set :following, User
+  set :followers, User
   set :keys, Key
+  set :repositories, Repository
 
   index :name
 
   def validate
     assert_present :name
+  end
+
+  class << self
+    # The method at the end of the authentication chain
+    def authenticate(username, password, options = {})
+      raise ConfigurationError, "No authentication method matched!"
+    end
+
+    # The method to be called if an authentication succeeded
+    def login(username)
+      User.find(:name => username).first || User.create(:name => username)
+    end
   end
 end
