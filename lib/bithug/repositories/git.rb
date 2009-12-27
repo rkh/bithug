afadf863185d391c7a429b170a2e60d822a8489c
Tim Felgentreff <timfelgentreff@gmail.com>
2009-12-27 13:30:43 +0100

    namespacing and re-naming

---
+++ lib/bithug/repositories/git.rb
@@ -0,0 +1,20 @@
+module Bithug::Repositories
+  module Git
+    def create_repository
+      super if vcs != "git"
+      git = Bithug::Git.new(absolute_path)
+      git.init
+    end
+
+    def remove_repository
+      super if vcs != "git"
+      git = Bithug::Git.new(absolute_path)
+      git.remove
+    end
+
+    private
+      def absolute_path
+        "#{Pathname.expand_path("~")}/#{name}"
+      end
+  end
+end
