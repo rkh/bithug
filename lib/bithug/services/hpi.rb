09df2a4462d56ae823cec1e71c6ec12ad7affe86
Tim Felgentreff <timfelgentreff@gmail.com>
2009-12-27 15:13:29 +0100

    poking around

--- lib/bithug/services/hpi.rb
+++ lib/bithug/services/hpi.rb
@@ -1,3 +1,4 @@
+# A configuration of Bithug for the HPI
 module Bithug::Hpi
 
   class Repository < Bithug::Repository
@@ -6,12 +7,11 @@ module Bithug::Hpi
   end
   
   class User < Bitug::User
+    attribute :real_name
+    attribute :email
+  
     include Bithug::Authentication::Local
     include Bithug::Authentication::LDAP
     include Bithug::Authentication::Kerberos
-
-    def email
-      "#{name}@hpi.uni-potsdam.de"
-    end
   end
 end
