ce875f784097066fd11d867decae8a5a55a5c9af
Tim Felgentreff <timfelgentreff@gmail.com>
2009-12-27 15:05:10 +0100

    re-order module inclusion. the last included module is called first

--- lib/bithug/services/hpi.rb
+++ lib/bithug/services/hpi.rb
@@ -1,14 +1,16 @@
 module Bithug::Hpi
+
   class Repository
     include Bithug::Repository
-    include Bithug::GitRepository
-    include Bithug::SvnRepository
+    include Bithug::Repositories::Svn
+    include Bithug::Repositories::Git
   end
 
   class User
     include Bithug::User
-    include Bithug::Authentication::Kerberos
+    include Bithug::Authentication::Local
     include Bithug::Authentication::LDAP
+    include Bithug::Authentication::Kerberos
 
     def email
       "#{name}@hpi.uni-potsdam.de"
