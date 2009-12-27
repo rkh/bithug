553a5d41fe45e0d57002bc64216b885c19b0407a
Tim Felgentreff <timfelgentreff@gmail.com>
2009-12-27 14:26:46 +0100

    adjust home-routes to new modularization

--- routes/home.rb
+++ routes/home.rb
@@ -1,42 +1,41 @@
 module Bithug
   class Routes < Sinatra::Base
-
-    def manager
-      AccessManager.new(current_user)
+  
+    helpers do
+      def current_user
+        User.find(:name => params[:username]).first
+      end
     end
 
-    get '/' do      
-      redirect "/#{current_user}"
+    get '/' do
+      redirect "/#{current_user}" 
     end
 
-    get '/:username/?' do      
-      haml :home, {}, :user => User.find(:name => params[:username]).first,
-	 :repositories => (Repository.find(:owner => params[:username]) || []),
-	 :writeable_repositories => Repository.writeable_repos_for_user(params[:username]),
-	 :readable_repositories => Repository.readable_repos_for_user(params[:username]),
-	 :following => (User.find(:name => current_user).first.following.include? params[:username])
+    get '/:username/?' do
+      user = current_user
+      haml :home, {}, 
+          :user => user,
+          :repositories => user.repositories,
+          :writeable_repositories => Repository.writeable_repos(user),
+          :readable_repositories => Repository.readable_repos(user),
+          :following => current_user.following.include?(user)
     end
 
     post "/:username/?" do
-      require 'pp'; pp params
-      if params["post"]["key"] 
-	new_key = params["post"]["key"]
-        manager.add_key(new_key)
-      end
-      if params["post"]["follow"]
-	user = User.find(:name, current_user).first
-	user.following << params[:username]
-	user.save
-	pp user.following
-      end
-      if params["post"]["unfollow"]
-	user = User.find(:name, current_user).first
-	user.following.delete(params[:username])
-	user.save
-	pp user.following
+      if params["post"]["key"]
+        Key.add(:user => current_user, 
+                :name => params["post"]["name"],
+                :value => params["post"]["key"])
+      elsif params["post"]["follow"]
+        user = current_user
+        user.following << params[:username]
+        user.save
+	  elsif params["post"]["unfollow"]
+	    user = current_user
+        user.following.delete(params[:username])
+        user.save
       end
       redirect request.path_info
     end
-
   end
 end
