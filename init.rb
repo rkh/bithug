caa652a5b45d375ce1e1c3288dca66e34632e74d
Tim Felgentreff <timfelgentreff@gmail.com>
2010-01-24 19:11:43 +0100

    use config

--- init.rb
+++ init.rb
@@ -3,6 +3,7 @@ ROOT_DIR = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift("lib", *Dir.glob(File.join(ROOT_DIR, "vendor", "*", "lib")))
 
 require "bithug/webserver"
+require "config"
 
 Bithug::Webserver.configure do |app|
   app.app_file = __FILE__
