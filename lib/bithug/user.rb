fd78126ace343e4f1a66aee2d6243214b5a8c02b
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-01-24 15:57:45 +0100

    fix autoloader

--- lib/bithug/user.rb
+++ lib/bithug/user.rb
@@ -1,14 +1,20 @@
 require "bithug"
 
 module Bithug
+  
+  class AbstractUser < Ohm::Model
+  end
+  
+  class User < AbstractUser
+  end
 
   # A user of Bithug - nice and pretty
   class AbstractUser < Ohm::Model
     attribute :name
-    set :following, User
-    set :followers, User
-    set :keys, Key
-    set :repositories, Repository
+    set :following, Bithug::User
+    set :followers, Bithug::User
+    set :keys, Bithug::Key
+    set :repositories, Bithug::Repository
 
     index :name
 
@@ -22,11 +28,8 @@ module Bithug
 
     # The method to be called if an authentication succeeded
     def self.login(username)
-      User.find(:name => username).first || User.create(:name => username)
+      Bithug::User.find(:name => username).first || Bithug::User.create(:name => username)
     end
-  end
-
-  class User < AbstractUser
   end
 
 end
