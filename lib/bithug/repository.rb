76e8f2520cdc0db41225d0d25e1f92802d624f28
Tim Felgentreff <timfelgentreff@gmail.com>
2010-01-24 16:31:43 +0100

    use the AbstractRepo > Repo (as we do in User) now, also add accessors for the owner, as it has to be a typed collection with Ohm

--- lib/bithug/repository.rb
+++ lib/bithug/repository.rb
@@ -1,75 +1,94 @@
-class Bithug::Repository < Ohm::Model
-  attribute :name
-  attribute :public
-  attribute :owner
-  attribute :vcs
-  set :readers
-  set :writers
+module Bithug
+  class AbstractRepository < Ohm::Model
+    attribute :name
+    attribute :public
+    attribute :vcs
+    set :owner, Bithug::User
+    set :readers, Bithug::User
+    set :writers, Bithug::User
 
-  index :name
-  index :owner
+    index :name
+    index :owner
 
-  def validate
-    assert_present :name
-    assert_present :vcs
-  end
-  
-  def create_repository
-    raise ConfigurationError, "#{vcs} is an unhandled VCS"
-  end
+    def validate
+      assert_present :name
+      assert_present :vcs
+    end
+    
+    def create_repository
+      raise ConfigurationError, "#{vcs} is an unhandled VCS"
+    end
 
-  def remove_repository
-    raise ConfigurationError, "#{vcs} is an unhandled VCS"
-  end
+    def remove_repository
+      raise ConfigurationError, "#{vcs} is an unhandled VCS"
+    end
 
-  # This is used by the shell
-  def check_access_rights(user, writeaccess=false)
-    user_name = username(user)
-    unless self.owner == user_name
-      unless self.readaccess.include?(user_name) || (self.public == true)
-        raise ReadAccessDeniedError, 
-            "#{self.owner} User #{user_name} does not have read-access"
-      else
-        unless (self.writeaccess.include?(user_name) || !writeaccess)
-          raise WriteAccessDeniedError, 
-              "User #{user_name} does not have write-access"
+    # This is used by the shell
+    def check_access_rights(user, writeaccess=false)
+      user_name = username(user)
+      unless self.owner == user_name
+        unless self.readaccess.include?(user_name) || (self.public == true)
+          raise ReadAccessDeniedError, 
+              "#{self.owner} User #{user_name} does not have read-access"
+        else
+          unless (self.writeaccess.include?(user_name) || !writeaccess)
+            raise WriteAccessDeniedError, 
+                "User #{user_name} does not have write-access"
+          end
         end
       end
     end
-  end
 
-  class << self
-    # Return all repositories that are writeable by the given user
-    def writeable_repos(user)
-      all.select do |repo|
-        repo.writeaccess.include?(user)
+    class << self
+      # Return all repositories that are writeable by the given user
+      def writeable_repos(user)
+        all.select do |repo|
+          repo.writeaccess.include?(user)
+        end
+      end
+
+      # Return all repositories that are readeable by the given user
+      def readable_repos(user)
+        all.select do |repo|
+          repo.readaccess.include?(user)
+        end
+      end
+
+      # This is overwritten to immediately create the underlying repo using the 
+      # configured method, and also modify the name for uniqueness in the system 
+      def create(*args)
+        creation_hash = args.first.merge(:name => "#{args.first[:owner].name}/#{args.first[:name]}")
+        owner = creation_hash.delete(:owner)
+        repo = super(creation_hash)
+        repo.create_repository
+        repo.owner = owner
+        repo.save
+        owner.repositories << repo
+        owner.save
+        repo
+      end
+
+      # Ohm only provides class matching for sets. Owner should be 
+      # only one, anyhow, so provide accessors...  
+      def owner=(user)
+        owner.clear
+        owner << user
+      end
+
+      def owner
+        owner.first
+      end
+
+      # This is overwritten to actually remove the repository from storage or 
+      # whatever is configured on delete
+      def delete
+        remove_repository
+        super
       end
     end
-
-    # Return all repositories that are readeable by the given user
-    def readable_repos(user)
-      all.select do |repo|
-        repo.readaccess.include?(user)
-      end
-    end
-
-    # This is overwritten to immediately create the underlying repo using the 
-    # configured method, and also modify the name for uniqueness in the system 
-    def create(*args)
-      hash = args.first.merge(:name => "#{hash[:owner].name}/#{hash[:name]}")
-      repo = super(hash)
-      repo.create_repository
-      repo.save
-      hash[:owner].repositories << repo
-      hash[:owner].save
-      repo
-    end
-
-    # This is overwritten to actually remove the repository from storage or 
-    # whatever is configured on delete
-    def delete
-      remove_repository
-      super
-    end
   end
+
+  class Repository < AbstractRepository
+  end
+
 end
