c3944ffdac5c4aab3e47d57cb0739d3b08ff2b62
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-01-24 15:36:07 +0100

    new monkey-lib version

--- dependencies.rb
+++ dependencies.rb
@@ -1,9 +1,6 @@
 require "depgen/depgen"
 class Dependencies < Depgen
-  github "lsegal", "yard", :version => "0.5.2"
+  github "lsegal", "yard", :version => "0.5.3"
   github "dchelimsky", "rspec", :version => "1.3.0"
-  github("rkh", :git_ref => "v%s") do
-    add "monkey-lib", :version => "0.3.5"
-    add "big_band", :version => "0.2.5"
-  end
+  github "rkh", "big_band", :version => "0.2.5", :git_ref => "v%s"
 end
--- lib/bithug/webserver.rb
+++ lib/bithug/webserver.rb
@@ -3,5 +3,13 @@ require "sinatra/big_band"
 
 module Bithug
   class Webserver < Sinatra::BigBand
+    
+    helpers do
+    end
+    
+    get "/" do
+      "hello"
+    end
+    
   end
 end
