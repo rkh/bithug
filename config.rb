251b2c23bb4323fde373a75a0a05976dc51655e6
Tim Felgentreff <timfelgentreff@gmail.com>
2010-01-24 21:04:39 +0100

    add datamapper config

--- config.rb
+++ config.rb
@@ -1,5 +1,15 @@
+require 'dm-core'
+
 Bithug.configure do
   # use Twitter, :except => Repository
   # use Ldap, :host => "ldap.com", :port => 10
   use :Hpi
+
+  # An in-memory Sqlite3 connection:
+  DataMapper.setup(:default, 'sqlite3::memory:')
+  # A MySQL connection:
+  #DataMapper.setup(:default, 'mysql://localhost/the_database_name')
+  # A Postgres connection:
+  #DataMapper.setup(:default, 'postgres://localhost/the_database_name')
 end
+
