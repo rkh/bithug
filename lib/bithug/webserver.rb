660a8b26cb9022490e7850193abf9546444564f7
Tim Felgentreff <timfelgentreff@gmail.com>
2010-01-24 17:09:05 +0100

    add basic auth and a #current_user helper

--- lib/bithug/webserver.rb
+++ lib/bithug/webserver.rb
@@ -3,12 +3,21 @@ require "sinatra/big_band"
 
 module Bithug
   class Webserver < Sinatra::BigBand
+
+    use Rack::Auth::Basic do |username, password|
+      if User.authenticate(username, password)
+        session["user"] = User.login(username).name
+      end
+    end
     
     helpers do
+      def current_user
+        User.find(:name => session["user"]).first
+      end
     end
     
     get "/" do
-      "hello"
+      "Hello #{current_user.name}"
     end
     
   end
