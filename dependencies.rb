a8e02fe12f537e11da190d3abc6a53380a3f4581
Tim Felgentreff <timfelgentreff@gmail.com>
2010-01-24 20:19:55 +0100

    add krb5-auth, net-ldap

--- dependencies.rb
+++ dependencies.rb
@@ -3,4 +3,6 @@ class Dependencies < Depgen
   github "lsegal", "yard", :version => "0.5.3"
   github "dchelimsky", "rspec", :version => "1.3.0"
   github "rkh", "big_band", :version => "0.2.5", :git_ref => "v%s"
+  github "timfel", "krb5-auth", :version => "0.8", :git_ref => "v%s"
+  gem "net-ldap"
 end
