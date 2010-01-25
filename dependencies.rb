12c592aa07da2a3183d690af8c1d140b209b9295
Tim Felgentreff <timfelgentreff@gmail.com>
2010-01-25 11:02:11 +0100

    more dependencies

--- dependencies.rb
+++ dependencies.rb
@@ -3,6 +3,7 @@ class Dependencies < Depgen
   github "lsegal", "yard", :version => "0.5.3"
   github "dchelimsky", "rspec", :version => "1.3.0"
   github "rkh", "big_band", :version => "0.2.5", :git_ref => "v%s"
-  github "timfel", "krb5-auth", :version => "0.8", :git_ref => "v%s"
-  gem "net-ldap"
+  add "net-ldap"
+  add "timfel-krb5-auth"
+  add "bcrypt"
 end
