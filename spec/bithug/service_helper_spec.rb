29bd98520ea834732961e562f0fe8e6552636baa
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-01-28 11:49:35 +0100

    dispatch spec

--- spec/bithug/service_helper_spec.rb
+++ spec/bithug/service_helper_spec.rb
@@ -1,1 +1,39 @@
 require File.expand_path('../../spec_helper', __FILE__)
+
+describe Bithug::ServiceHelper do
+  
+  def service(&block)
+    Module.new { include Bithug::ServiceHelper }.tap { |m| m.class_eval(&block) if block }
+  end
+  
+  def service_method(name, &block)
+    service { define_method(name, &block) }
+  end
+
+  def service_class_method(name, &block)
+    service { class_methods { define_singleton_method(name, &block) } }
+  end
+  
+  it 'should dispatch instance methods in correct order' do
+    first_service  = service_method(:something) { [:foo] + super }
+    second_service = service_method(:something) { [:bar] + super }
+    bottom_service = service_method(:something) { [:blah] }
+    klass = Class.new
+    [first_service, second_service, bottom_service].reverse_each do |service|
+      klass.send :include, service
+    end
+    klass.new.something.should == [:foo, :bar, :blah]
+  end
+  
+  it 'should dispatch class methods in correct order' do
+    first_service  = service_class_method(:something) { [:foo] + super }
+    second_service = service_class_method(:something) { [:bar] + super }
+    bottom_service = service_class_method(:something) { [:blah] }
+    klass = Class.new
+    [first_service, second_service, bottom_service].reverse_each do |service|
+      klass.send :include, service
+    end
+    klass.something.should == [:foo, :bar, :blah]
+  end
+  
+end
