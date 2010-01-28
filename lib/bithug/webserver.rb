4db44d7345d97be59d417495fce2817bd3762955
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-01-28 11:54:19 +0100

    white space

--- lib/bithug/webserver.rb
+++ lib/bithug/webserver.rb
@@ -11,21 +11,21 @@ module Bithug
         current_user = Bithug::User.login(username).name
       end
     end
-    
+
     helpers do
       def current_user
         Bithug::User.find(:name => request.env['REMOTE_USER']).first
       end
 
       def current_user=(user)
-	user = user.name unless user.respond_to? :to_str
-	session[:user] = user
+        user = user.name unless user.respond_to? :to_str
+        session[:user] = user
       end
     end
-    
+
     get "/" do
       "Hello #{current_user.name}"
     end
-    
+
   end
 end
