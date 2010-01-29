afd346181e78b533660daf3477904b888a610ef2
Tim Felgentreff <timfelgentreff@gmail.com>
2010-01-29 16:38:06 +0100

    need a module to wrap it up

---
+++ lib/bithug/log_info.rb
@@ -0,0 +1,8 @@
+# This module wraps up the different kinds
+# of logs we keep 
+module Bithug::LogInfo
+  Bithug::LogInfo::CommitInfo
+  Bithug::LogInfo::FollowInfo
+  Bithug::LogInfo::RightsInfo 
+  Bithug::LogInfo::ForkInfo 
+end
