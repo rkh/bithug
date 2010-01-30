8ab927169cc0324378c21854bad07cdcb7f7aa9d
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-01-30 18:55:37 +0100

    defaults for hpi

--- lib/bithug/hpi.rb
+++ lib/bithug/hpi.rb
@@ -8,11 +8,23 @@ module Bithug::Hpi
 
   module User
     include Bithug::ServiceHelper 
-    stack Bithug::Kerberos::User, Bithug::Local::User#, Bithug::Ldap::User
-    
-    def login(username)
-      super
-      self.real_name = username.split(".").join " "
+    stack Bithug::Local::User, Bithug::Kerberos::User #, Bithug::Ldap::User
+
+    def login(email)
+      username = user_from_mail email
+      super(username).tap do |user|
+        user.real_name = username.split(".").map { |s| s.capitalize }.join " "
+        user.email = email
+      end
+    end
+
+    def authenticate(email, *whatever)
+      username = user_from_mail(email) or return false
+      super(username, *whatever)
+    end
+
+    def user_from_mail(email)
+      $1.downcase if email =~ /^([^@]+\.[^@]+)@(student\.)?hpi\.uni-potsdam\.de$/
     end
 
   end
