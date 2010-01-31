2cc3d077d448ea3dafcf1fdd5f25f1b0243775a6
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-01-31 13:06:25 +0100

    fix hpi

--- lib/bithug/hpi.rb
+++ lib/bithug/hpi.rb
@@ -10,21 +10,25 @@ module Bithug::Hpi
     include Bithug::ServiceHelper 
     stack Bithug::Local::User, Bithug::Kerberos::User #, Bithug::Ldap::User
 
-    def login(email)
-      username = user_from_mail email
-      super(username).tap do |user|
-        user.real_name = username.split(".").map { |s| s.capitalize }.join " "
-        user.email = email
+    class_methods do
+
+      def login(email)
+        username = user_from_mail email
+        super(username).tap do |user|
+          user.real_name = username.split(".").map { |s| s.capitalize }.join " "
+          user.email = email
+        end
       end
-    end
 
-    def authenticate(email, *whatever)
-      username = user_from_mail(email) or return false
-      super(username, *whatever)
-    end
+      def authenticate(email, *whatever)
+        username = user_from_mail(email) or return false
+        super(username, *whatever)
+      end
 
-    def user_from_mail(email)
-      $1.downcase if email =~ /^([^@]+\.[^@]+)@(student\.)?hpi\.uni-potsdam\.de$/
+      def user_from_mail(email)
+        $1.downcase if email =~ /^([^@]+\.[^@]+)@(student\.)?hpi\.uni-potsdam\.de$/
+      end
+
     end
 
   end
