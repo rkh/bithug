465239f31168bdf15b73a156a9bf32109086109f
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-02-05 20:30:26 +0100

    documentation

--- lib/bithug/config_dsl.rb
+++ lib/bithug/config_dsl.rb
@@ -6,11 +6,37 @@ class Bithug::ConfigDsl
     @environment = env.to_sym
   end
 
+  ##
+  # @overload environment
+  #   @return [Symbol]
+  #     the current environment
+  #
+  # @overload environment(*envs)
+  #   @param [Enumerable<Symbol, #to_sym>] *envs
+  #     list of environments for which the given configuration should apply
+  #   @yield
+  #     configuration for the given envs
   def environment(*envs)
     yield if envs.any? { |e| e.to_sym == @environment }
     @environment
   end
-  
+
+  ##
+  # @example Mapping an array
+  #   map_arguments [1, 2], :foo, :bar, :blah               # => { :foo => 1,  :bar  => 2  }
+  #   map_arguments [1, {:blah => 3}], :foo, :bar, :blah    # => { :foo => 1,  :blah => 2  }
+  #   map_arguments [{:foo => 10}], :foo => 20, :bar => 30  # => { :foo => 10, :bar  => 30 }
+  #
+  # @example Using for unifying method arguments
+  #   def connect(*args)
+  #     options = map_arguments(args, :host, :port, :host => "localhost", :port => 80, :ssl => false)
+  #     make_request options
+  #   end
+  #
+  # @param [Enumerable<Hash, Object>] list
+  #   Arguments to map
+  # @param [Enumerable<#to_sym, Hash>] *keys
+  #   Keys for merging list elements into the result. Last element can be a hash with default values.
   def map_arguments(list, *keys)
     defaults = keys.pop if keys.last.is_a? Hash
     list.inject(defaults || {}) do |hash, value|
